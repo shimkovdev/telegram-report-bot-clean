0b4f216f3562952951fb9279ba53149393ca9d3f
shimkovdev <aleksandrshimkov@gmail.com>
2025-05-20 15:23:31 +0200

    Update googleDrive.js

--- googleDrive.js
+++ googleDrive.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const path = require('path');
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS || './GOOGLE_APPLICATION_CREDENTIALS',
+  keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   scopes: ['https://www.googleapis.com/auth/drive']
 });
 
